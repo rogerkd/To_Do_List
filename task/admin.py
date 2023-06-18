471a900275cf9b8441d30ce1f64ab830924d8449
Deepak Kumar <deepakt90su30@gmail.com>
2023-06-19 02:56:35 +0530

    reverse commit

--- task/admin.py
+++ task/admin.py
@@ -1,24 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import User, Group
-from .models import Profile, Task
+from .models import Task
 
-admin.site.unregister(Group)
 admin.site.register(Task)
 
-# Combine profile to user
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    
-
-# Extend User Model
-class UserAdmin(admin.ModelAdmin):
-    model = User
-    fields = ['username','first_name','last_name','password','email']
-    inlines = [ProfileInline]
-
-
-# Unregister Initial User
-admin.site.unregister(User)
-
-# Register user and profile
-admin.site.register(User, UserAdmin)
--- task/models.py
+++ task/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True, verbose_name='Profile Picture')
-    bio = models.TextField(blank=True, null=True)
-    following = models.ManyToManyField(
-        "self",
-        related_name="follower",
-        symmetrical=False,
-        blank=True
-    )
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
